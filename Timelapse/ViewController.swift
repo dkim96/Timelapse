3885481bf06b731403013f94cde98aecde3d2ca1
Daniel Kim <danielk@daniels-macbook-pro.local>
2017-09-25 11:19:46 -0700

    Initial Commit

---
+++ Timelapse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timelapse
+//
+//  Created by Daniel Kim on 9/25/17.
+//  Copyright © 2017 dk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
